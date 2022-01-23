a1618b53c34e9498ed2cf8bacb120cad53fb6915
LautaroLamalfa <79757905+LautaroLamalfa@users.noreply.github.com>
2022-01-23 20:48:58 -0300

    Mas mas cambios

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,13 @@ const { Router } = express;
 const router = new Router();
 
 router.get("/", (req, res) => {
-    res.sendfile('public/login.html', { root: "."})
+    res.sendFile('public/login.html', { root: "."})
 });
 
 router.post("/", (req, res) => {
     req.session.user = req.body.nombre;
     res.statusCode = 302;
-    res.setHeader("Location", "http://localhost:8080");
+    res.setHeader("Location", "http://localhost:8081");
     res.end()
 })
 
